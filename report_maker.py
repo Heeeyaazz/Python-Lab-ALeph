3a4d86db04a385b7e115ba182b7280764534db21
Heeeyaazz <bboongbbaang12@gmail.com>
2026-09-07 10:41:16 +0900

    자동 스케줄러 n8n Webhook에 production URL로 변경

--- report_maker.py
+++ report_maker.py
@@ -12,7 +12,7 @@ import requests  # pip install requests
 # 각자 본인 n8n 웹훅 주소를 넣으세요.
 # 환경변수 N8N_WEBHOOK_URL 이 설정돼 있으면 그 값을 우선 사용합니다.
 # WEBHOOK_URL = os.getenv("N8N_WEBHOOK_URL", "")   # ← 여기 또는 환경변수에
-WEBHOOK_URL = "http://localhost:5678/webhook-test/94bf9165-6b66-4438-8bfa-8272be1b2983"
+WEBHOOK_URL = "http://localhost:5678/webhook/a750d7c8-225f-4e85-acb5-a3adf9dcae56"
 
 BASE_DIR = Path(__file__).resolve().parent       # 스크립트가 있는 폴더 기준
 REPORT_PATH = BASE_DIR / "report.md"
