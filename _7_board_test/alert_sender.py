e031ca615eb4411122d5a4ff6d323e5c1607c2be
Heeeyaazz <bboongbbaang12@gmail.com>
2026-09-15 09:31:01 +0900

    Update alert sender and add Graylog configuration

--- _7_board_test/alert_sender.py
+++ _7_board_test/alert_sender.py
@@ -1,6 +1,6 @@
 import requests
 
-N8N_URL = "http://localhost:5678/webhook-test/a750d7c8-225f-4e85-acb5-a3adf9dcae56"
+N8N_URL = "http://localhost:5678/webhook-test/607c9793-d61f-47c1-9e33-0c8664c224c3"
 STUDENT = "심희현"
 
 data = {
